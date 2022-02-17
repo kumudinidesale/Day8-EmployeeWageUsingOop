e7b031c2ba68a785f2b8b06669103434c25c1c5e
kumudinidesale <kdesale1211@gmail.com>
2022-02-17 15:16:45 +0530

    added:for multiple companies

--- src/com/java/EmployeeWage.java
+++ src/com/java/EmployeeWage.java
@@ -1,58 +1,45 @@
 package com.java;
 
-import java.util.Random;
-
 public class EmployeeWage {
 
-	public static final int IS_PART_TIME = 1;
-	public static final int IS_FULL_TIME = 2;
+	public static void calculatingEmployeeWage(String company, int empRatePerHour, int numberOfWorkingDays,
+			int maxHoursPerMonth) {
+		int empWorkingHour = 0;
+		int totalWorkingPerHour = 0;
+		int totalWorkingDay = 0;
 
-	private String company = "";
-	private int empRatePerHour = 0;
-	private int numOfWorkingDays = 0;
-	private int maxHoursPerMonth = 0;
+		while (totalWorkingDay <= numberOfWorkingDays && totalWorkingPerHour < empRatePerHour) {
+			totalWorkingDay++;
+			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
 
-	public EmployeeWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
-		super();
-		this.company = company;
-		this.empRatePerHour = empRatePerHour;
-		this.numOfWorkingDays = numOfWorkingDays;
-		this.maxHoursPerMonth = maxHoursPerMonth;
-	}
-
-	public static void totalEmpWage() {
-		EmployeeWage Bazar = new EmployeeWage("Bazaar", 40, 20, 100);
-		Random random = new Random();
-		int totalWorkingDays = 0;
-		int totalWorkingHours = 0;
-		int empWage = 0;
-		int totalEmpWage = 0;
-
-		while (totalWorkingDays < Bazar.numOfWorkingDays && totalWorkingHours < Bazar.maxHoursPerMonth) {
-			totalWorkingDays++;
-			int empCheck = random.nextInt(3);
-			int empHrs = 0;
 			switch (empCheck) {
-
-			case IS_PART_TIME -> empHrs = 4;
-
-			case IS_FULL_TIME -> empHrs = 8;
-
-			default -> empHrs = 0;
-
+			case 1:
+				empWorkingHour = 8;
+				break;
+			case 2:
+				empWorkingHour = 4;
+				break;
+			default:
+				empWorkingHour = 0;
+				break;
 			}
-			totalWorkingHours = empHrs + totalWorkingHours;
-			empWage = empHrs * Bazar.empRatePerHour;
-			System.out.println("day " + totalWorkingDays + " = " + empWage);
-			totalEmpWage = empWage + totalEmpWage;
+
+			totalWorkingPerHour += empWorkingHour;
+			System.out.println("Day :" + totalWorkingDay);
+			System.out.println("Total Employee working hour  is  : " + empWorkingHour);
 
 		}
-		System.out.println("salary of " + Bazar.company + " employee is " + totalEmpWage);
+		int totalEmployeewage = totalWorkingPerHour * empRatePerHour;
+		System.out.println("Total Employee Wage is : " + totalEmployeewage);
+		System.out.println("Total working hours are  : " + totalWorkingPerHour);
 
 	}
 
 	public static void main(String[] args) {
-		totalEmpWage();
 
+		System.out.println("UPL : ");
+		calculatingEmployeeWage("UPL ", 120, 30, 10);
+		System.out.println("\nTCS : ");
+		calculatingEmployeeWage("TCS ", 125, 8, 80);
 	}
 }
